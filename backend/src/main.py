3cf424a88ab764317e8be2e12383bea0221efc1e
Kam Wai Chan <danny24981@hotmail.com>
2023-11-12 11:03:20 -0500

    small update

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from fastapi.responses import RedirectResponse
 
 from sqlalchemy.orm import Session
 
-#from cas import CASClient
+from cas import CASClient
 
 from enums import *
 from datetime import date
@@ -46,7 +46,41 @@ def read_root():
 
 SERVICE_URL = "https://bktp-gradpro.discovery.cs.vt.edu/"
 
+# Creating the CAS CLIENT
+cas_client = CASClient(
+    version=2,
+    service_url=f"{SERVICE_URL}/api/login?",
+    server_url="https://login.vt.edu/profile/cas/",
+    # CHANGE: If you want VT CS CAS, to be used instead of VT CAS
+    # change the server_url to https://login.cs.vt.edu/cas/
+)
 
+# Routes related to CAS
+@app.get("/api/login")
+def login(request: Request):
+
+    username = request.cookies.get("username")
+    if username: # return user info
+        return {"message": "Logged in!"}
+
+    cas_ticket = request.query_params.get("ticket")
+    if not cas_ticket:
+        cas_login_url = cas_client.get_login_url()
+        return {"redirect_url": cas_login_url}
+
+    (user, _, _) = cas_client.verify_ticket(cas_ticket)
+    if not user:
+        raise HTTPException(status_code=403, detail="Failed to verify ticket!")
+
+    response = RedirectResponse(SERVICE_URL)
+    response.set_cookie(key="username", value=user)
+    return response
+
+@app.get("/api/logout")
+def logout(response: Response):
+    cas_logout_url = cas_client.get_logout_url(SERVICE_URL)
+    response.delete_cookie("username")
+    return {"redirect_url": cas_logout_url}
 
 #------------------- non-cas --------------------#
 
