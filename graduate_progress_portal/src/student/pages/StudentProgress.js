fa27828c1fefa3a1b20bfb34f0e1e5036225a4c8
ben0002 <bennyweng520@gmail.com>
2023-11-17 00:31:48 -0500

    fixed for real this time, studentProgress

--- graduate_progress_portal/src/student/pages/StudentProgress.js
+++ graduate_progress_portal/src/student/pages/StudentProgress.js
@@ -9,6 +9,46 @@ import StudentFundingCard from '../components/StudentFundingCard.js';
 import StudentEmploymentCard from '../components/StudentEmploymentCard.js';
 import StudentCourseHistoryCard from '../components/StudentCourseHistoryCard.js';
 
+// const StudentProgress = () => {
+//   return (
+//     <Box sx={{ width: '65%', mx: 'auto' }}> {/* Adjust this width for the global container */}
+//       {/* Margin for cards */}
+//       <Grid container spacing={2}>
+//         {/* StudentCard and StudentProfileCard */}
+//         <Grid item xs={12}>
+//           <StudentCardInfo student={studentData} />
+//         </Grid>
+//         <Grid item xs={12}>
+//           <ToDoList student={studentData.tasks} />
+//         </Grid>
+
+//         {/* Milestones */}
+//         <Grid item xs={12} md={5}>
+//           <StudentMilestoneCard student={studentData.milestones} />
+//         </Grid>
+
+//         {/* Requirements */}
+//         <Grid item xs={12} md={5}>
+//           <StudentRequirementCard student={studentData.requirements} />
+//         </Grid>
+
+//         {/* Funding and Employment with Divider */}
+//         <Grid item xs={12} md={2} sx={{ display: 'flex', flexDirection: 'column' }}>
+//           <StudentFundingCard student={studentData.funding} />
+//           <Divider />
+//           <StudentEmploymentCard student={studentData.employment} />
+//         </Grid>
+
+//         {/* Course History */}
+//         <Grid item xs={10}>
+//           <StudentCourseHistoryCard student={studentData.courseHistory} />
+//         </Grid>
+
+//       </Grid>
+//     </Box>
+//   );
+// };
+
 const StudentProgress = () => {
   return (
     <Box sx={{ width: '65%', mx: 'auto' }}> {/* Adjust this width for the global container */}
@@ -21,12 +61,6 @@ const StudentProgress = () => {
         <Grid item xs={12}>
           <ToDoList student={studentData.tasks} />
         </Grid>
-<<<<<<< HEAD
-
-        {/* Milestones */}
-        <Grid item xs={12} md={5}>
-          <StudentMilestoneCard student={studentData.milestones} />
-=======
         <Grid container item spacing={2}>
           <Grid item xs={10}>
             <Grid container spacing={2}>
@@ -60,29 +94,9 @@ const StudentProgress = () => {
               </Grid>
             </Grid>
           </Grid>
->>>>>>> 1991dd3 (Seperated Funding and Employment)
         </Grid>
-
-        {/* Requirements */}
-        <Grid item xs={12} md={5}>
-          <StudentRequirementCard student={studentData.requirements} />
-        </Grid>
-
-        {/* Funding and Employment with Divider */}
-        <Grid item xs={12} md={2} sx={{ display: 'flex', flexDirection: 'column' }}>
-          <StudentFundingCard student={studentData.funding} />
-          <Divider />
-          <StudentEmploymentCard student={studentData.employment} />
-        </Grid>
-
-        {/* Course History */}
-        <Grid item xs={10}>
-          <StudentCourseHistoryCard student={studentData.courseHistory} />
-        </Grid>
-
       </Grid>
     </Box>
   );
-};
-
+}
 export default StudentProgress;
