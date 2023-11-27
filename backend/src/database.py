604b1402110e0cae76bf311abe79b13b21f6a574
ben0002 <bennyweng520@gmail.com>
2023-11-26 19:14:11 -0500

    changed database config back to original

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,4 +1,4 @@
-''' The central file for our database setup.'''
+''' The central file for our database setup. '''
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -8,19 +8,14 @@ if os.environ.get("ENVIRONMENT") != "production":
     from dotenv import load_dotenv
     load_dotenv()
 
-# Database configuration, if the environment variables are not set, use the default values
-host = os.environ.get("DB_HOST", "default_host") 
-user = os.environ.get("DB_USER", "default_user")
-password = os.environ.get("DB_PASSWORD", "default_password")
-database = os.environ.get("DB_NAME", "default_database")
-port = os.environ.get("DB_PORT", "3306") 
+host = os.environ.get("DB_HOST")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
+database = os.environ.get("DB_NAME")
+port = os.environ.get("DB_PORT")
 
-# Construct the database URL
 URL_DATABASE = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
-try:
-    engine = create_engine(URL_DATABASE)
+engine = create_engine(URL_DATABASE)
 
-    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-except Exception as e:
-    print(f"Error setting up the database: {e}")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
