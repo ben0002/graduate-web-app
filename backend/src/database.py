51d80c30451b6a93fbe89cc3110a3b9c1162d34e
ben0002 <113151296+ben0002@users.noreply.github.com>
2023-11-26 19:54:34 -0500

    Update database.py

--- backend/src/database.py
+++ backend/src/database.py
@@ -8,11 +8,11 @@ if os.environ.get("ENVIRONMENT") != "production":
     from dotenv import load_dotenv
     load_dotenv()
 
-host = os.environ.get("DB_HOST")
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
-database = os.environ.get("DB_NAME")
-port = os.environ.get("DB_PORT")
+host = os.environ.get("DB_HOST", "default_host")
+user = os.environ.get("DB_USER", "default_user")
+password = os.environ.get("DB_PASSWORD", "")
+database = os.environ.get("DB_NAME", "default_database")
+port = os.environ.get("DB_PORT", "3306")
 
 URL_DATABASE = f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
 
